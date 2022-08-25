0c54dd80c2b56ff11db454382c4f45e499badcd3
Tucker Steil <tuckersteil@gmail.com>
2022-08-25 15:55:17 -0700

    commit tree a6d98e11f98e776d2cc8389cac1a50b060957b58

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Mr.Chocolate", 12, "M", "black", "social", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Rowdy", 11, "M", "brown", "friendly", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Tabitha", 3, "F", "red", "social", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Sergeant Brown", 17, "M", "black", "friendly", false);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Melissa", 9, "M", "grey", "dangerous", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Grinch", 10, "F", "brown", "mean", true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ("Wendy", 11, "F", "red", "outgoing", false);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES (NULL, 14, "M", "black", "social", true);
